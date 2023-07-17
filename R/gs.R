fc2791be85ec1370c7b7952fa013145505508c4a
benjaminmisiuk <benjaminmisiuk@gmail.com>
2023-07-17 11:34:32 -0300

    Create gs.R

---
+++ R/gs.R
@@ -0,0 +1,14 @@
+#' Bay of Fundy Sediments
+#'
+#' Sediment grain size data and environmental variables from the Bay of Fundy, Canada
+#'
+#' @docType data
+#'
+#' @usage data(gs)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(gs)
